22cd6ff5a991cb227c4473fa0fe1154743ddabcb
rk2105 <57768129+rk2105@users.noreply.github.com>
2024-10-14 22:12:43 +0530

    Add files via upload

---
+++ strings/usr_strchr.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdio_ext.h>
+char *_mystrchr(char *ptr,char ch);
+int main()
+{
+	char str[20],ch;
+	printf("Enter the string ");
+	//gets(str);
+	scanf("%[^\n]s",str);
+	__fpurge(stdin);
+	char *ptr=str;
+	printf("Enter char to search in string ");
+	scanf("%c",&ch);
+	//char *ptr=str;
+	ptr=_mystrchr(ptr,ch);
+	if(ptr==NULL)
+	{
+		printf("Not Found");
+	}
+	else
+	{
+		printf("Found at %ld",ptr-str);
+	}
+}
+char *_mystrchr(char *ptr,char ch)
+{
+	while((*ptr)!='\0')
+	{
+		if((*ptr)==ch)
+		{
+			return ptr;
+		}
+		ptr++;
+	}
+	return NULL;
+}
